3fea07ecf32fb39a1ca3c3962e666e405dc1b534
Bitmaker Labs <bitmakerlabs@Bitmaker-3.local>
2017-04-13 10:35:01 -0400

    Add scopes to Track model

--- chinook_app/app/models/track.rb
+++ chinook_app/app/models/track.rb
@@ -1,2 +1,14 @@
 class Track < ActiveRecord::Base
+
+  SHORT = 180000
+  LONG  = 360000
+
+  scope :short,  -> { shorter_than_or_equal_to(SHORT) }
+  scope :medium, -> { where('milliseconds > ? AND milliseconds < ?', SHORT, LONG) }
+  scope :long,   -> { where('milliseconds >= ?', LONG) }
+
+  scope :shorter_than_or_equal_to, -> (milliseconds) { where('milliseconds <= ?', milliseconds)}
+
+  scope :starts_with, -> (first_char) { where('name ILIKE ?', "#{ first_char}%") }
+
 end
