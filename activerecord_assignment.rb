2318a9f92509e823c16c3c731b448789f68b9e18
Leon <leon.karho.chung@gmail.com>
2017-04-20 17:43:43 -0400

    1a, 1b, 2

--- activerecord_assignment.rb
+++ activerecord_assignment.rb
@@ -5,15 +5,21 @@
 
 
 # 1a) Find the genre with the name "Hip Hop/Rap".
-
+Genre.find_by(name: 'Hip Hop/Rap')
+  Genre Load (0.7ms)  SELECT  "genres".* FROM "genres" WHERE "genres"."name" = $1 LIMIT $2  [["name", "Hip Hop/Rap"], ["LIMIT", 1]]
+=> #<Genre id: 17, name: "Hip Hop/Rap", created_at: "2013-01-20 02:04:31", updated_at: "2014-01-29 22:14:08">
 
 
 # 1b) Count how many tracks belong to the "Hip Hop/Rap" genre
-
+Track.where(genre_id: '17').count
+   (1.8ms)  SELECT COUNT(*) FROM "tracks" WHERE "tracks"."genre_id" = $1  [["genre_id", 17]]
+=> 35
 
 
 # 2) Find the total amount of time required to listen to all the tracks in the database.
-
+Track.sum('milliseconds')
+SELECT SUM("tracks"."milliseconds") FROM "tracks"
+=> 1378575827
 
 
 # 3a) Find the highest price of any track that has the media type "MPEG audio file".
